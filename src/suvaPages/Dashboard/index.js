82c5015c807d7b3918c9a02e7cd5cc3864f848c4
sarthakshrm-dev <sarthakshrm2001@gmail.com>
2023-12-07 22:23:06 +0530

    dashboard

--- src/suvaPages/Dashboard/index.js
+++ src/suvaPages/Dashboard/index.js
@@ -1,7 +1,41 @@
-import React from 'react'
+import React from 'react';
+import { Col, Container, Row } from 'reactstrap';
 
-export default function Dashboard() {
-  return (
-    <div>Dashboard</div>
-  )
-}
+//import Components
+import UsersByDevice from './UsersByDevice';
+import Widget from './Widget';
+import TopPages from './TopPages';
+import ReservationStatistics from './ReservationStatistics';
+import Rooms from './Rooms';
+import {Button} from 'reactstrap';
+
+const DashboardAnalytics = () => {
+    document.title = "Analytics | Velzon - React Admin & Dashboard Template";
+    return (
+        <React.Fragment>
+            <div className="page-content">
+                <Container fluid>
+                    <div className='w-100 d-flex align-items-center justify-content-between mb-4'>
+                        <h3 className='d-flex'>Dashboard</h3>
+                        <Button color="danger"> Add new property </Button>
+                    </div>
+                    <Row>
+                        <Col md={12}>
+                            <Widget />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <ReservationStatistics />
+                        <UsersByDevice />
+                    </Row>
+                    <Row>
+                        <TopPages />
+                        <Rooms />
+                    </Row>
+                </Container>
+            </div>
+        </React.Fragment>
+    );
+};
+
+export default DashboardAnalytics;
